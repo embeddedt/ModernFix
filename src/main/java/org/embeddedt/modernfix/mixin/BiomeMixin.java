f2e557253ecb6b2808ba66e5722b06a7142bb4eb
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-01-02 10:25:39 -0500

    Remove biome temperature caching
    Based on https://github.com/CaffeineMC/lithium-fabric/commit/4553ccb0a02049c588fbacd689afbace067add82

---
+++ src/main/java/org/embeddedt/modernfix/mixin/BiomeMixin.java
@@ -0,0 +1,24 @@
+package org.embeddedt.modernfix.mixin;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.biome.Biome;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+
+/* Idea from Lithium for 1.19.3 */
+@Mixin(Biome.class)
+public abstract class BiomeMixin {
+    @Shadow protected abstract float getTemperatureAtPosition(BlockPos pos);
+
+    /**
+     * @author 2No2Name
+     * @reason Remove caching, it's not effective
+     * @param pos
+     * @return
+     */
+    @Overwrite
+    public final float getTemperature(BlockPos pos) {
+        return this.getTemperatureAtPosition(pos);
+    }
+}

50fb19156c33ca72f1982e1b38a489898d86a98e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-01-04 14:24:33 -0500

    Time game startup as well

--- src/main/java/org/embeddedt/modernfix/ModernFixClient.java
+++ src/main/java/org/embeddedt/modernfix/ModernFixClient.java
@@ -2,15 +2,20 @@ package org.embeddedt.modernfix;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.ConnectingScreen;
+import net.minecraft.client.gui.screen.MainMenuScreen;
 import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+import java.lang.management.ManagementFactory;
+
 public class ModernFixClient {
 
     public static long worldLoadStartTime;
     private static int numRenderTicks;
+
+    public static float gameStartTimeSeconds = -1;
 
     public void resetWorldLoadStateMachine() {
         numRenderTicks = 0;
@@ -21,6 +26,9 @@ public class ModernFixClient {
     public void onMultiplayerConnect(GuiOpenEvent event) {
         if(event.getGui() instanceof ConnectingScreen && !event.isCanceled()) {
             worldLoadStartTime = System.nanoTime();
+        } else if (event.getGui() instanceof MainMenuScreen && gameStartTimeSeconds < 0) {
+            gameStartTimeSeconds = ManagementFactory.getRuntimeMXBean().getUptime() / 1000f;
+            ModernFix.LOGGER.warn("Game took " + gameStartTimeSeconds + " seconds to start");
         }
     }
 
@@ -29,6 +37,7 @@ public class ModernFixClient {
         if(event.phase == TickEvent.Phase.END && worldLoadStartTime != -1 && Minecraft.getInstance().player != null && numRenderTicks++ >= 10) {
             float timeSpentLoading = ((float)(System.nanoTime() - worldLoadStartTime) / 1000000000f);
             ModernFix.LOGGER.warn("Time from main menu to in-game was " + timeSpentLoading + " seconds");
+            ModernFix.LOGGER.warn("Total time to load game and open world was " + (timeSpentLoading + gameStartTimeSeconds) + " seconds");
             resetWorldLoadStateMachine();
         }
     }

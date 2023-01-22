8cdc425d7dd51fb338db7e56e5e78cd8026542cc
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-01-22 18:07:48 -0500

    Ensure the main thread does not spin when no tasks are available

--- src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
+++ src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
@@ -30,7 +30,7 @@ public class ModernFixEarlyConfig {
         this.addMixinRule("perf.async_jei", true);
         this.addMixinRule("perf.thread_priorities", true);
         this.addMixinRule("perf.preload_block_classes", false);
-        this.addMixinRule("perf.defer_voxelshape_optimize", false);
+        this.addMixinRule("perf.sync_executor_sleep", true);
         this.addMixinRule("perf.parallel_blockstate_cache_rebuild", true);
         this.addMixinRule("perf.deduplicate_location", true);
         this.addMixinRule("safety", true);
---
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/sync_executor_sleep/SyncExecutorMixin.java
@@ -0,0 +1,30 @@
+package org.embeddedt.modernfix.mixin.perf.sync_executor_sleep;
+
+import net.minecraftforge.fml.ModWorkManager;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+@Mixin(targets = "net/minecraftforge/fml/ModWorkManager$SyncExecutor")
+public class SyncExecutorMixin {
+    @Shadow private ConcurrentLinkedDeque<Runnable> tasks;
+    private static final long PARK_TIME = TimeUnit.MILLISECONDS.toNanos(50);
+    /**
+     * Currently FML spins in driveOne while waiting for the modloading workers. We can improve this
+     * by sleeping for 50ms at a time.
+     * @author embeddedt
+     * @reason improve CPU efficiency
+     */
+    @Inject(method = "driveOne", at = @At("HEAD"), remap = false)
+    private void sleepWhileNoTasks(CallbackInfoReturnable<Boolean> cir) {
+        if(tasks.isEmpty())
+            LockSupport.parkNanos(PARK_TIME);
+    }
+}

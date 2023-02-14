d06ba2b7bf64f476843a50c7b0fe40bc6aa4636d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-02-14 15:27:57 -0500

    Make DummySearchTree extend SearchTreeReloadable for compatibility

--- src/main/java/org/embeddedt/modernfix/searchtree/DummySearchTree.java
+++ src/main/java/org/embeddedt/modernfix/searchtree/DummySearchTree.java
@@ -1,14 +1,23 @@
 package org.embeddedt.modernfix.searchtree;
 
 import net.minecraft.client.util.IMutableSearchTree;
+import net.minecraft.client.util.SearchTree;
+import net.minecraft.client.util.SearchTreeReloadable;
+import net.minecraft.util.ResourceLocation;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * Dummy search tree that stores nothing and returns nothing on searches.
  */
-public class DummySearchTree<T> implements IMutableSearchTree<T> {
+public class DummySearchTree<T> extends SearchTreeReloadable<T> implements IMutableSearchTree<T> {
+    public DummySearchTree() {
+        super(t -> Stream.empty());
+    }
+
     @Override
     public void add(T pObj) {
 

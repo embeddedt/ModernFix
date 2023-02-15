381f6a5f1ecd9c58ace0071a5482578211fe69ed
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-02-15 18:10:42 -0500

    Fix JEI mixins

--- src/main/java/org/embeddedt/modernfix/mixin/perf/async_jei/IngredientListElementFactoryMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/async_jei/IngredientListElementFactoryMixin.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(IngredientListElementFactory.class)
 public class IngredientListElementFactoryMixin {
     private static int ingredientNum = 0;
-    @Inject(method = "addToBaseList", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/NonNullList;add(Ljava/lang/Object;)Z"), remap = false)
+    @Inject(method = "addToBaseList", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/NonNullList;add(Ljava/lang/Object;)Z"), remap = false)
     private static void checkForInterrupt(NonNullList<IIngredientListElement<?>> baseList, IIngredientManager ingredientManager, IIngredientType ingredientType, CallbackInfo ci) {
         if((ingredientNum++ % 100) == 0)
             IAsyncJeiStarter.checkForLoadInterruption();

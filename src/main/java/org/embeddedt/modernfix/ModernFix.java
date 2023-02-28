9902b97b1d860b524808f85702b4313cb68c8c71
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-02-27 20:58:46 -0500

    Warn if LazyDFU is not installed

--- src/main/java/org/embeddedt/modernfix/ModernFix.java
+++ src/main/java/org/embeddedt/modernfix/ModernFix.java
@@ -3,14 +3,13 @@ package org.embeddedt.modernfix;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.ExtensionPoint;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartedEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.network.FMLNetworkConstants;
 import org.apache.commons.lang3.tuple.Pair;
@@ -65,11 +64,30 @@ public class ModernFix {
         INSTANCE = this;
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::commonSetup);
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> MinecraftForge.EVENT_BUS.register(new ModernFixClient()));
         ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ModernFixConfig.COMMON_CONFIG);
         if(ModList.get().isLoaded("kubejs"))
             MinecraftForge.EVENT_BUS.register(KubeUtil.class);
+    }
+
+    private static boolean dfuModPresent() {
+        for(String modId : new String[] { "lazydfu", "datafixerslayer" }) {
+            if(ModList.get().isLoaded(modId))
+                return true;
+        }
+        return false;
+    }
+
+    @SubscribeEvent
+    public void commonSetup(FMLCommonSetupEvent event) {
+        if(!dfuModPresent()) {
+            event.enqueueWork(() -> {
+                ModLoader.get().addWarning(new ModLoadingWarning(ModLoadingContext.get().getActiveContainer().getModInfo(), ModLoadingStage.COMMON_SETUP, "modernfix.no_lazydfu"));
+            });
+        }
+
     }
 
     @SubscribeEvent

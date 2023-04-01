ba82be7a8910bd6f4f0f9306372283d19ac956d2
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-01 19:53:19 -0400

    Log on search tree replacement

--- src/main/java/org/embeddedt/modernfix/mixin/perf/blast_search_trees/MinecraftMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/blast_search_trees/MinecraftMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.searchtree.SearchRegistry;
 import net.minecraftforge.fml.ModContainer;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.forgespi.language.IModFileInfo;
+import org.embeddedt.modernfix.ModernFix;
 import org.embeddedt.modernfix.searchtree.DummySearchTree;
 import org.embeddedt.modernfix.searchtree.JEIBackedSearchTree;
 import org.embeddedt.modernfix.searchtree.REIBackedSearchTree;
@@ -26,12 +27,15 @@ public class MinecraftMixin {
         ci.cancel();
         Optional<? extends ModContainer> jeiContainer = ModList.get().getModContainerById("jei");
         if(ModList.get().isLoaded("roughlyenoughitems")) {
+            ModernFix.LOGGER.info("Replaced creative search logic with REI");
             this.searchRegistry.register(SearchRegistry.CREATIVE_NAMES, list -> new REIBackedSearchTree(false));
             this.searchRegistry.register(SearchRegistry.CREATIVE_TAGS, list -> new REIBackedSearchTree(true));
         } else if(jeiContainer.isPresent() && jeiContainer.get().getModInfo().getVersion().getMajorVersion() >= 10) {
+            ModernFix.LOGGER.info("Replaced creative search logic with JEI");
             this.searchRegistry.register(SearchRegistry.CREATIVE_NAMES, list -> new JEIBackedSearchTree(false));
             this.searchRegistry.register(SearchRegistry.CREATIVE_TAGS, list -> new JEIBackedSearchTree(true));
         } else {
+            ModernFix.LOGGER.info("Replaced creative search logic with dummy implementation");
             this.searchRegistry.register(SearchRegistry.CREATIVE_NAMES, list -> new DummySearchTree<>());
             this.searchRegistry.register(SearchRegistry.CREATIVE_TAGS, list -> new DummySearchTree<>());
         }

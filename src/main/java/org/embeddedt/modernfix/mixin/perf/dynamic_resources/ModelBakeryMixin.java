4f17319836d3c549ac504797ffd56d5a074a936f
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-11 11:16:07 -0400

    Fix compile error

--- src/main/java/org/embeddedt/modernfix/mixin/perf/dynamic_resources/ModelBakeryMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/dynamic_resources/ModelBakeryMixin.java
@@ -177,7 +177,7 @@ public abstract class ModelBakeryMixin {
         gatherModelMaterials(materialSet);
     }
 
-    @Redirect(method = "processLoading", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", ordinal = 0))
+    @Redirect(method = "processLoading", at = @At(value = "INVOKE", target = "Ljava/util/Map;forEach(Ljava/util/function/BiConsumer;)V", ordinal = 0), remap = false)
     private void fetchStaticDefinitions(Map<ResourceLocation, StateDefinition<Block, BlockState>> map, BiConsumer<ResourceLocation, StateDefinition<Block, BlockState>> func) {
         map.forEach((loc, def) -> blockStateFiles.add(loc));
     }

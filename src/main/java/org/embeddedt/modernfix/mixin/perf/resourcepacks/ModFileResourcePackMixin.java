1602d3352f19befb138d08889f904cdfed819388
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-16 14:38:11 -0400

    Fix crash with some more types of empty paths

--- src/main/java/org/embeddedt/modernfix/mixin/perf/resourcepacks/ModFileResourcePackMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/resourcepacks/ModFileResourcePackMixin.java
@@ -86,6 +86,8 @@ public abstract class ModFileResourcePackMixin {
             return false;
         }
         String str = path.toString();
+        if(str.length() == 0)
+            return false;
         for(int i = 0; i < str.length(); i++) {
             if(!ResourceLocation.validPathChar(str.charAt(i))) {
                 return false;

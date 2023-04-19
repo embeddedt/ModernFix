201c2f3b51bc3d164a6265aac2963f51929a7135
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-18 22:02:06 -0400

    Add off-by-default option to disable the DFU maps for flattening

--- src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
+++ src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
@@ -41,6 +41,7 @@ public class ModernFixEarlyConfig {
         /* Use a simpler ArrayMap if FerriteCore is using the map intelligently anyway */
         this.addMixinRule("perf.state_definition_construct", modPresent("ferritecore"));
         this.addMixinRule("perf.cache_strongholds", true);
+        this.addMixinRule("perf.dedup_blockstate_flattening_map", false);
         this.addMixinRule("perf.cache_upgraded_structures", true);
         this.addMixinRule("perf.biome_zoomer", true);
         this.addMixinRule("perf.compress_blockstate", false);

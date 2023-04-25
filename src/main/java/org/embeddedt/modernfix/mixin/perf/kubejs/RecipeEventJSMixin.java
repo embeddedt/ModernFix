e843f8ed6d567acb67a7bb728c9fbd1e45d48132
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-25 19:29:26 -0400

    Clear KubeJS recipe event lists since mods can hold onto the event object

--- src/main/java/org/embeddedt/modernfix/mixin/perf/kubejs/RecipeEventJSMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/kubejs/RecipeEventJSMixin.java
@@ -7,6 +7,7 @@ import dev.latvian.kubejs.recipe.RecipeJS;
 import dev.latvian.kubejs.recipe.filter.RecipeFilter;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.RecipeManager;
+import org.embeddedt.modernfix.ModernFix;
 import org.embeddedt.modernfix.util.KubeUtil;
 import org.embeddedt.modernfix.util.ModUtil;
 import org.spongepowered.asm.mixin.Final;
@@ -17,6 +18,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
@@ -50,5 +54,38 @@ public class RecipeEventJSMixin {
     @Inject(method = "post(Lnet/minecraft/world/item/crafting/RecipeManager;Ljava/util/Map;)V", at = @At("RETURN"))
     private void clearRecipeRegistry(RecipeManager manager, Map<ResourceLocation, JsonObject> jsonMap, CallbackInfo ci) {
         KubeUtil.originalRecipesByHash.clear();
+    }
+
+    /**
+     * The recipe event object can be leaked in scripts and this wastes 40MB of memory.
+     */
+    @Inject(method = "post", at = @At("RETURN"))
+    private void clearRecipeLists(CallbackInfo ci) {
+        ModernFix.LOGGER.info("Clearing KubeJS recipe lists...");
+        // Even though we are a mixin class, use reflection so this works across a variety of versions
+        Field[] fields = RecipeEventJS.class.getDeclaredFields();
+        for(Field f : fields) {
+            try {
+                if(!Modifier.isStatic(f.getModifiers())
+                        && (Collection.class.isAssignableFrom(f.getType())
+                            || Map.class.isAssignableFrom(f.getType()))
+                ) {
+                    f.setAccessible(true);
+                    Object collection = f.get(this);
+                    int size;
+                    if(collection instanceof Map) {
+                        size = ((Map<?, ?>)collection).size();
+                        ((Map<?, ?>)collection).clear();
+                    } else if(collection instanceof Collection) {
+                        size = ((Collection<?>)collection).size();
+                        ((Collection<?>)collection).clear();
+                    } else
+                        throw new IllegalStateException();
+                    ModernFix.LOGGER.debug("Cleared {} with {} entries", f.getName(), size);
+                }
+            } catch(RuntimeException | ReflectiveOperationException e) {
+                ModernFix.LOGGER.debug("Uh oh, couldn't clear field", e);
+            }
+        }
     }
 }

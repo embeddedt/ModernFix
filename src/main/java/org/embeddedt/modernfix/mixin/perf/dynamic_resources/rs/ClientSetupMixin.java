4006551afecd56178ab62198c37d542b469e4f79
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-04-29 11:36:59 -0400

    Adjust mixin for 1.18

--- src/main/java/org/embeddedt/modernfix/mixin/perf/dynamic_resources/rs/ClientSetupMixin.java
+++ src/main/java/org/embeddedt/modernfix/mixin/perf/dynamic_resources/rs/ClientSetupMixin.java
@@ -15,15 +15,15 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientSetup.class)
 public class ClientSetupMixin {
-    @Shadow @Final private BakedModelOverrideRegistry bakedModelOverrideRegistry;
+    @Shadow @Final private static BakedModelOverrideRegistry BAKED_MODEL_OVERRIDE_REGISTRY;
 
-    @Inject(method = "<init>", at = @At("RETURN"))
-    private void addDynamicListener(CallbackInfo ci) {
-        MinecraftForge.EVENT_BUS.addListener(this::onDynamicModelBake);
+    @Inject(method = "onClientSetup", at = @At("RETURN"), remap = false)
+    private static void addDynamicListener(CallbackInfo ci) {
+        MinecraftForge.EVENT_BUS.addListener(ClientSetupMixin::onDynamicModelBake);
     }
 
-    private void onDynamicModelBake(DynamicModelBakeEvent event) {
-        BakedModelOverrideRegistry.BakedModelOverrideFactory factory = bakedModelOverrideRegistry.get(event.getLocation() instanceof ModelResourceLocation ? new ResourceLocation(event.getLocation().getNamespace(), event.getLocation().getPath()) : event.getLocation());
+    private static void onDynamicModelBake(DynamicModelBakeEvent event) {
+        BakedModelOverrideRegistry.BakedModelOverrideFactory factory = BAKED_MODEL_OVERRIDE_REGISTRY.get(event.getLocation() instanceof ModelResourceLocation ? new ResourceLocation(event.getLocation().getNamespace(), event.getLocation().getPath()) : event.getLocation());
         if(factory != null)
             event.setModel(factory.create(event.getModel(), event.getModelLoader().getBakedTopLevelModels()));
     }

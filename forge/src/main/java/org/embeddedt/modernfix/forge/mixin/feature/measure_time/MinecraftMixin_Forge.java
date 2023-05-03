2aacadaa82314680a8e7964866aeeb78951ff6b0
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-02 20:58:52 -0400

    Fix incorrectly mapped mixin

--- forge/src/main/java/org/embeddedt/modernfix/forge/mixin/feature/measure_time/MinecraftMixin_Forge.java
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/feature/measure_time/MinecraftMixin_Forge.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Minecraft.class)
 @ClientOnlyMixin
 public class MinecraftMixin_Forge {
-    @Inject(method = "doWorldLoad", at = @At("HEAD"), remap = false)
+    @Inject(method = "doWorldLoad", at = @At("HEAD"))
     private void recordWorldLoadStart(CallbackInfo ci) {
         ModernFixClient.worldLoadStartTime = System.nanoTime();
     }

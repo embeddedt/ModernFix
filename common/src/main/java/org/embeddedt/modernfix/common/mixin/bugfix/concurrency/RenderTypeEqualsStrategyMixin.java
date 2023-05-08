33f689ba9aa56b2b5e533b8473b55308497006db
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-08 16:15:15 -0400

    Make RenderType EqualsStrategy more predictable

---
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/bugfix/concurrency/RenderTypeEqualsStrategyMixin.java
@@ -0,0 +1,17 @@
+package org.embeddedt.modernfix.common.mixin.bugfix.concurrency;
+
+import net.minecraft.client.renderer.RenderType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Objects;
+
+@Mixin(targets = { "net/minecraft/client/renderer/RenderType$CompositeRenderType$EqualsStrategy"})
+public class RenderTypeEqualsStrategyMixin {
+    @Redirect(method = "equals(Lnet/minecraft/client/renderer/RenderType$CompositeRenderType;Lnet/minecraft/client/renderer/RenderType$CompositeRenderType;)Z", at = @At(value = "INVOKE", target = "Ljava/util/Objects;equals(Ljava/lang/Object;Ljava/lang/Object;)Z", ordinal = 0))
+    private boolean alsoCheckName(Object a, Object b, RenderType.CompositeRenderType type1, RenderType.CompositeRenderType type2) {
+        boolean supposedlyEqual = Objects.equals(a, b);
+        return supposedlyEqual && Objects.equals(type1.name, type2.name);
+    }
+}

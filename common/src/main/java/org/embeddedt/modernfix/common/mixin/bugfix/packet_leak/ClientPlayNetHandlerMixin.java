b21ee9a7e721f92aa0a8a1a44d79f871fdb1131e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-05-14 19:59:39 -0400

    Hopefully more stable version of the original packet fix

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/bugfix/packet_leak/ClientPlayNetHandlerMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/bugfix/packet_leak/ClientPlayNetHandlerMixin.java
@@ -11,23 +11,13 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientPacketListener.class)
 @ClientOnlyMixin
-public class ClientPlayNetHandlerMixin implements IClientNetHandler {
-    private FriendlyByteBuf savedCopy = null;
+public class ClientPlayNetHandlerMixin {
     /**
      * @author embeddedt
-     * @reason Release the packet buffer at the end. Needed in f
+     * @reason allow the other function to track use of the buffer
      */
     @Redirect(method = "handleCustomPayload", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/protocol/game/ClientboundCustomPayloadPacket;getData()Lnet/minecraft/network/FriendlyByteBuf;"))
     private FriendlyByteBuf saveCopyForRelease(ClientboundCustomPayloadPacket instance) {
-        FriendlyByteBuf copy = instance.getData();
-        savedCopy = copy;
-        return copy;
-    }
-
-    @Override
-    public FriendlyByteBuf getCopiedCustomBuffer() {
-        FriendlyByteBuf copy = savedCopy;
-        savedCopy = null;
-        return copy;
+        return ((IClientNetHandler)instance).getCopiedCustomBuffer();
     }
 }

bac13236e7d49dd1c5c797cfe7cee0a97c126479
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-06-26 12:57:49 -0400

    Use WeakReference for server variable but never clear it for better mod compat

--- fabric/src/main/java/org/embeddedt/modernfix/ModernFixFabric.java
+++ fabric/src/main/java/org/embeddedt/modernfix/ModernFixFabric.java
@@ -4,22 +4,23 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.minecraft.server.MinecraftServer;
 
+import java.lang.ref.WeakReference;
+
 public class ModernFixFabric implements ModInitializer {
     private ModernFix commonMod;
-    public static MinecraftServer theServer;
+    public static WeakReference<MinecraftServer> theServer = new WeakReference<>(null);
     @Override
     public void onInitialize() {
         commonMod = new ModernFix();
 
         ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-            theServer = server;
+            theServer = new WeakReference<>(server);
         });
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
             commonMod.onServerStarted();
         });
         ServerLifecycleEvents.SERVER_STOPPED.register(server -> {
             commonMod.onServerDead(server);
-            theServer = null;
         });
 
         // TODO: implement entity ID desync

33e437f8653e68aa1ac4fb679fc95871794a34bf
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-26 21:29:33 -0400

    Hotfix for CoFH crash
    Apparently BooleanSupplier is *not* used

--- forge/src/main/java/org/embeddedt/modernfix/forge/mixin/bugfix/cofh_core_crash/FlagManagerMixin.java
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/bugfix/cofh_core_crash/FlagManagerMixin.java
@@ -2,12 +2,15 @@ package org.embeddedt.modernfix.forge.mixin.bugfix.cofh_core_crash;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import org.embeddedt.modernfix.annotation.RequiresMod;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Coerce;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.function.BooleanSupplier;
+import java.util.function.Supplier;
 
 /**
  * Fix getOrCreateFlag accessing the FLAGS map without synchronization by wrapping all calls to it
@@ -18,15 +21,15 @@ import java.util.function.BooleanSupplier;
 @RequiresMod("cofh_core")
 public class FlagManagerMixin {
     @Shadow @Final
-    private static Object2ObjectOpenHashMap<String, BooleanSupplier> FLAGS;
+    private static Object2ObjectOpenHashMap<String, Supplier<Boolean>> FLAGS;
 
     @Shadow
-    private BooleanSupplier getOrCreateFlag(String flag) {
+    private Supplier<Boolean> getOrCreateFlag(String flag) {
         throw new AssertionError();
     }
 
     @Redirect(method = "*", at = @At(value = "INVOKE", target = "getOrCreateFlag"), require = 0)
-    private BooleanSupplier getFlag(@Coerce Object flagHandler, String flag) {
+    private Supplier<Boolean> getFlag(@Coerce Object flagHandler, String flag) {
         if(flagHandler != this)
             throw new AssertionError("Redirect targeted bad getOrCreateFlag invocation");
         synchronized (FLAGS) {

23e768d62944480b1b7b87235e442cf5bbc0538b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-28 16:09:21 -0400

    Spotless

--- fabric/src/test/java/net/minecraft/world/level/block/state/BlockStateCacheTest.java
+++ fabric/src/test/java/net/minecraft/world/level/block/state/BlockStateCacheTest.java
@@ -1,7 +1,6 @@
 package net.minecraft.world.level.block.state;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.level.EmptyBlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;

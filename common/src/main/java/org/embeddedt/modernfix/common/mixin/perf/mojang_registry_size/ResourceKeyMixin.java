3e4f1ab23aebb0436261e8c7bcf2f91a67f9878f
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-07-31 13:13:39 -0400

    Improve registry performance with large entry counts

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/mojang_registry_size/ResourceKeyMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/mojang_registry_size/ResourceKeyMixin.java
@@ -1,4 +1,4 @@
-package org.embeddedt.modernfix.forge.mixin.perf.fast_registry_validation;
+package org.embeddedt.modernfix.common.mixin.perf.mojang_registry_size;
 
 import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.resources.ResourceKey;
@@ -12,7 +12,7 @@ import java.util.Map;
 
 @Mixin(ResourceKey.class)
 public class ResourceKeyMixin<T> {
-    private static Map<ResourceLocation, Map<ResourceLocation, ResourceKey<?>>> INTERNING_MAP = new Object2ObjectOpenHashMap<>();
+    private static final Map<ResourceLocation, Map<ResourceLocation, ResourceKey<?>>> INTERNING_MAP = new Object2ObjectOpenHashMap<>();
     @Inject(method = "create(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/resources/ResourceKey;", at = @At("HEAD"), cancellable = true)
     private static <T> void createEfficient(ResourceLocation parent, ResourceLocation location, CallbackInfoReturnable<ResourceKey<T>> cir) {
         synchronized (ResourceKey.class) {

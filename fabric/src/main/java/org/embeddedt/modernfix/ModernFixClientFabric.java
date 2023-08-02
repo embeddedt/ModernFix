7e777625a5e2ab26c2fd1557f0dade04517c2b5d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-08-02 19:05:39 -0400

    Remove dependency on fabric-lifecycle-events

--- fabric/src/main/java/org/embeddedt/modernfix/ModernFixClientFabric.java
+++ fabric/src/main/java/org/embeddedt/modernfix/ModernFixClientFabric.java
@@ -1,8 +1,6 @@
 package org.embeddedt.modernfix;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 
 public class ModernFixClientFabric implements ClientModInitializer {
     public static ModernFixClient commonMod;
@@ -10,10 +8,5 @@ public class ModernFixClientFabric implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         commonMod = new ModernFixClient();
-
-        ClientTickEvents.END_CLIENT_TICK.register((mc) -> commonMod.onRenderTickEnd());
-        ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            commonMod.onServerStarted(server);
-        });
     }
 }
---
+++ fabric/src/main/java/org/embeddedt/modernfix/fabric/mixin/core/ClientMinecraftServerMixin.java
@@ -0,0 +1,18 @@
+package org.embeddedt.modernfix.fabric.mixin.core;
+
+import net.minecraft.server.MinecraftServer;
+import org.embeddedt.modernfix.ModernFixClient;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+@ClientOnlyMixin
+public class ClientMinecraftServerMixin {
+    @Inject(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/Util;getMillis()J", ordinal = 0))
+    private void markServerStarted(CallbackInfo ci) {
+        ModernFixClient.INSTANCE.onServerStarted((MinecraftServer)(Object)this);
+    }
+}

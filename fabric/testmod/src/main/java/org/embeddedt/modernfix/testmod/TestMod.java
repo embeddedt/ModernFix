fed0392ddcf53d15e5d6ef7be1156a96b7681f44
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-08-09 20:07:59 -0400

    Introduce test mod

---
+++ fabric/testmod/src/main/java/org/embeddedt/modernfix/testmod/TestMod.java
@@ -0,0 +1,10 @@
+package org.embeddedt.modernfix.testmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class TestMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hello ModernFix world!");
+    }
+}

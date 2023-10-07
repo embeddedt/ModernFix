803aaba20497e938f2263ff5823db08f2a9db9d1
Phoenix-Starlight <64714532+Phoenix-Starlight@users.noreply.github.com>
2023-10-07 15:41:52 -0700

    Dynamic sound unloading (#234)

---
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_sounds/SoundBufferLibraryMixin.java
@@ -0,0 +1,44 @@
+package org.embeddedt.modernfix.common.mixin.perf.dynamic_sounds;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalNotification;
+import com.mojang.blaze3d.audio.SoundBuffer;
+import net.minecraft.client.sounds.SoundBufferLibrary;
+import net.minecraft.resources.ResourceLocation;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
+import org.embeddedt.modernfix.dynamicresources.DynamicSoundHelpers;
+import org.embeddedt.modernfix.ModernFix;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.Map;
+
+@Mixin(SoundBufferLibrary.class)
+@ClientOnlyMixin
+public abstract class SoundBufferLibraryMixin {
+
+    private static final boolean debugDynamicSoundLoading = Boolean.getBoolean("modernfix.debugDynamicSoundLoading");
+
+    @Shadow @Final @Mutable
+    private Map<ResourceLocation, CompletableFuture<SoundBuffer>> cache =
+    CacheBuilder.newBuilder()
+    .expireAfterAccess(DynamicSoundHelpers.MAX_SOUND_LIFETIME_SECS, TimeUnit.SECONDS)
+    // Excessive use of type hinting due to it assuming Object as the broadest correct type
+    .<ResourceLocation, CompletableFuture<SoundBuffer>>removalListener(this::onSoundRemoval)
+    .<ResourceLocation, CompletableFuture<SoundBuffer>>build()
+    .asMap();
+
+    private <K extends ResourceLocation, V extends CompletableFuture<SoundBuffer>> void onSoundRemoval(RemovalNotification<K, V> notification) {
+        notification.getValue().thenAccept(SoundBuffer::discardAlBuffer);
+        if(debugDynamicSoundLoading) {
+            K k = notification.getKey();
+            if(k == null)
+                return;
+            ModernFix.LOGGER.warn("Evicted sound {}", k);
+        }
+    }
+}
---
+++ common/src/main/java/org/embeddedt/modernfix/dynamicresources/DynamicSoundHelpers.java
@@ -0,0 +1,8 @@
+package org.embeddedt.modernfix.dynamicresources;
+
+public class DynamicSoundHelpers {
+	/**
+	 * The duration until a sound is eligible for eviction if unused.
+	 */
+	public static final int MAX_SOUND_LIFETIME_SECS = 120;
+}

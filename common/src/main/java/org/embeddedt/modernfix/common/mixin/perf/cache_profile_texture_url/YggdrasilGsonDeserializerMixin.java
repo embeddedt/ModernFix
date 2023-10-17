65201cbb02baee8b16c31262f4a6c25ada9aa64a
Phoenix-Starlight <novastarlight100@gmail.com>
2023-10-16 20:32:55 -0700

    Cache MC profile textures' URLs

---
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/cache_profile_texture_url/YggdrasilGsonDeserializerMixin.java
@@ -0,0 +1,35 @@
+package org.embeddedt.modernfix.common.mixin.perf.cache_profile_texture_url;
+
+import com.mojang.authlib.minecraft.MinecraftProfileTexture;
+import com.mojang.authlib.yggdrasil.YggdrasilMinecraftSessionService;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
+import org.embeddedt.modernfix.duck.ICachedProfileTexture;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(value=YggdrasilMinecraftSessionService.class, remap=false)
+@ClientOnlyMixin
+public abstract class YggdrasilGsonDeserializerMixin {
+	/**
+	 * @author Fury_Phoenix
+	 * @reason Unexpected deserialization
+	 * **/
+
+	@WrapOperation
+	(method = "getTextures",
+		at = @At(
+			value = "INVOKE",
+			target = "Lcom/mojang/authlib/minecraft/MinecraftProfileTexture;getUrl()Ljava/lang/String;"
+		)
+	)
+	private String setCachedURL(MinecraftProfileTexture texture, Operation<String> original) {
+		// Because we don't have it set from deserialization
+		String url = original.call(texture);
+		((ICachedProfileTexture)texture).setCachedHash(url);
+		return url;
+	}
+
+}

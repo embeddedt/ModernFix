f4ce7072a661d033d77aa3fcb116c6e6f28f71fe
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-10-25 15:03:16 -0400

    Enable dynamic sounds now that Guava issue is worked around

--- common/src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
+++ common/src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
@@ -162,7 +162,6 @@ public class ModernFixEarlyConfig {
     private static final ImmutableMap<String, Boolean> DEFAULT_SETTING_OVERRIDES = new DefaultSettingMapBuilder()
             .put("mixin.perf.dynamic_resources", false)
             .putConditionally(() -> !isFabric, "mixin.perf.async_jei", false)
-            .put("mixin.perf.dynamic_sounds", false)
             .put("mixin.perf.reuse_datapacks", false)
             .put("mixin.perf.dynamic_block_codecs", false)
             .put("mixin.feature.direct_stack_trace", false)

08d81b1aacda1d4e42729fd6fec8c451fc6a4a3b
Phoenix-Starlight <novastarlight100@gmail.com>
2023-10-28 21:02:48 -0700

    Fix 1.18 generics

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/inline_methods/BannerRendererMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/inline_methods/BannerRendererMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.blockentity.BannerRenderer;
 import net.minecraft.client.resources.model.Material;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.core.Holder;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.entity.BannerPattern;
 import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
@@ -31,7 +30,7 @@ public abstract class BannerRendererMixin {
 			target = "Lnet/minecraft/client/renderer/blockentity/BannerRenderer;renderPatterns(Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/minecraft/client/model/geom/ModelPart;Lnet/minecraft/client/resources/model/Material;ZLjava/util/List;)V"
 		)
 	)
-	private static void inlineRenderCanvas(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, ModelPart modelPart, Material material, boolean isBanner, List<Pair<Holder<BannerPattern>, DyeColor>> patterns) {
+	private static void inlineRenderCanvas(PoseStack poseStack, MultiBufferSource multiBufferSource, int light, int overlay, ModelPart modelPart, Material material, boolean isBanner, List<Pair<BannerPattern, DyeColor>> patterns) {
 		BannerRenderer.renderPatterns(poseStack, multiBufferSource, light, overlay, modelPart, material, isBanner, patterns, false);
 	}
 }

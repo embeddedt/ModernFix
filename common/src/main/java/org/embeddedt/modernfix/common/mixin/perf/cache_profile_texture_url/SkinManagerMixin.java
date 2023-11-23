7be382a3cedadf1665cf030ff2fbef5161ae7d5b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-11-23 10:14:48 -0500

    More ClientOnlyMixin

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/cache_profile_texture_url/SkinManagerMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/cache_profile_texture_url/SkinManagerMixin.java
@@ -4,6 +4,7 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.mojang.authlib.minecraft.MinecraftProfileTexture;
 import net.minecraft.client.resources.SkinManager;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,6 +14,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
 @Mixin(SkinManager.class)
+@ClientOnlyMixin
 public class SkinManagerMixin {
     @Unique
     private final Cache<MinecraftProfileTexture, String> mfix$hashCache = CacheBuilder.newBuilder()

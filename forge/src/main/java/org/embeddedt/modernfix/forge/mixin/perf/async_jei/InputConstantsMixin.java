244fb8b4ee74440f0a9534b559a5734c69126eb0
Fury_Phoenix <64714532+Phoenix-Starlight@users.noreply.github.com>
2023-12-10 22:30:38 -0800

    Fix Forge mixins

--- forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/async_jei/InputConstantsMixin.java
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/async_jei/InputConstantsMixin.java
@@ -2,6 +2,7 @@ package org.embeddedt.modernfix.forge.mixin.perf.async_jei;
 
 import com.mojang.blaze3d.platform.InputConstants;
 import com.mojang.blaze3d.systems.RenderSystem;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.embeddedt.modernfix.annotation.RequiresMod;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(InputConstants.class)
+@ClientOnlyMixin
 @RequiresMod("jei")
 public class InputConstantsMixin {
     @Redirect(method = "isKeyDown", at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwGetKey(JI)I", remap = false))
--- forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/async_locator/EyeOfEnderAccess.java
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/async_locator/EyeOfEnderAccess.java
@@ -1,11 +1,13 @@
 package org.embeddedt.modernfix.forge.mixin.perf.async_locator;
 
 import net.minecraft.world.entity.projectile.EyeOfEnder;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(EyeOfEnder.class)
+@ClientOnlyMixin
 public interface EyeOfEnderAccess {
-	@Accessor
-	void setLife(int life);
+    @Accessor
+    void setLife(int life);
 }

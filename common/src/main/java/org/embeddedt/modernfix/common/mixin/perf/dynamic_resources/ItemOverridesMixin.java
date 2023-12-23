94fbefd9019b2a8274dc5d19eb414fa77eaf1e4b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-23 15:57:12 -0500

    Update mixin

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_resources/ItemOverridesMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_resources/ItemOverridesMixin.java
@@ -1,7 +1,6 @@
 package org.embeddedt.modernfix.common.mixin.perf.dynamic_resources;
 
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.block.model.ItemOverride;
 import net.minecraft.client.renderer.block.model.ItemOverrides;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.entity.LivingEntity;
@@ -15,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ItemOverrides.class)
 public class ItemOverridesMixin {
     @Inject(method = "resolve", at = @At("RETURN"), cancellable = true)
-    private void getRealModel(BakedModel bakedModel, ItemStack itemStack, ClientLevel clientLevel, LivingEntity livingEntity, CallbackInfoReturnable<BakedModel> cir) {
+    private void getRealModel(BakedModel bakedModel, ItemStack stack, ClientLevel level, LivingEntity entity, int seed, CallbackInfoReturnable<BakedModel> cir) {
         BakedModel original = cir.getReturnValue();
         if(original instanceof ItemOverrideBakedModel) {
             ItemOverrideBakedModel override = (ItemOverrideBakedModel)original;

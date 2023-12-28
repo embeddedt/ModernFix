ae8cfbaa3d880a20b70a418f4ae276312fa30981
embeddedt <42941056+embeddedt@users.noreply.github.com>
2023-12-27 19:12:11 -0500

    Disable dynamic DFU when Litematica is installed

--- common/src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
+++ common/src/main/java/org/embeddedt/modernfix/core/config/ModernFixEarlyConfig.java
@@ -215,6 +215,7 @@ public class ModernFixEarlyConfig {
         disableIfModPresent("mixin.bugfix.remove_block_chunkloading", "performant");
         disableIfModPresent("mixin.bugfix.paper_chunk_patches", "c2me");
         disableIfModPresent("mixin.bugfix.preserve_early_window_pos", "better_loading_screen");
+        disableIfModPresent("mixin.perf.dynamic_dfu", "litematica");
         disableIfModPresent("mixin.perf.cache_strongholds", "littletiles", "c2me");
         // content overlap
         disableIfModPresent("mixin.perf.deduplicate_wall_shapes", "dashloader");

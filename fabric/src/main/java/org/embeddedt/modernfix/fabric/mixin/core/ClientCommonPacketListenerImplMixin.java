34bc295b3c8cad9c326113c062ab1b016bf46017
Fury_Phoenix <64714532+Phoenix-Starlight@users.noreply.github.com>
2023-12-28 09:41:37 -0500

    Mark some more mixins as ClientOnlyMixin

--- fabric/src/main/java/org/embeddedt/modernfix/fabric/mixin/core/ClientCommonPacketListenerImplMixin.java
+++ fabric/src/main/java/org/embeddedt/modernfix/fabric/mixin/core/ClientCommonPacketListenerImplMixin.java
@@ -2,12 +2,14 @@ package org.embeddedt.modernfix.fabric.mixin.core;
 
 import net.minecraft.client.multiplayer.ClientCommonPacketListenerImpl;
 import org.embeddedt.modernfix.ModernFixClientFabric;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientCommonPacketListenerImpl.class)
+@ClientOnlyMixin
 public class ClientCommonPacketListenerImplMixin {
     @Inject(method = "handleUpdateTags", at = @At("RETURN"))
     private void signalTags(CallbackInfo ci) {

6fa3e6421006883d293ea3d67a2377d7d508d548
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-01-12 15:18:11 -0500

    Lower priority of datapack reload exception mixins

--- forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/datapack_reload_exceptions/LootTableManagerMixin.java
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/datapack_reload_exceptions/LootTableManagerMixin.java
@@ -6,7 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(LootTables.class)
+@Mixin(value = LootTables.class, priority = 2000)
 public class LootTableManagerMixin {
     @Redirect(method = "*(Lnet/minecraft/resources/IResourceManager;Lcom/google/common/collect/ImmutableMap$Builder;Lnet/minecraft/util/ResourceLocation;Lcom/google/gson/JsonElement;)V",
             at = @At(value = "INVOKE", target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V", remap = false), require = 0)

6143d8a783adf77708f1cfb96dda39b40e672b2e
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-07 15:54:09 -0500

    spotless

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/reduce_blockstate_cache_rebuilds/BlockStateBaseMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/reduce_blockstate_cache_rebuilds/BlockStateBaseMixin.java
@@ -16,7 +16,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.Map;
 
 
 @Mixin(BlockBehaviour.BlockStateBase.class)

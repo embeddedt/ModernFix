feaf363fd89b7802be419aa836221b70317853d8
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-02-26 16:21:41 -0500

    Add oversimplified version of No Chat Reports
    This has no player-friendly UI whatsoever, doesn't work on
    enforce-secure-profile servers, and has had very limited testing.
    
    In exchange, it costs virtually nothing to maintain.

---
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/feature/remove_chat_signing/ProfileKeyPairManagerMixin.java
@@ -0,0 +1,23 @@
+package org.embeddedt.modernfix.common.mixin.feature.remove_chat_signing;
+
+import net.minecraft.client.multiplayer.ProfileKeyPairManager;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
+import org.embeddedt.modernfix.core.config.Option;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(ProfileKeyPairManager.class)
+@ClientOnlyMixin
+public class ProfileKeyPairManagerMixin {
+    /**
+     * @author embeddedt
+     * @reason never use the key pair
+     */
+    @Overwrite
+    private CompletableFuture<Optional<?>> readOrFetchProfileKeyPair(Optional<?> optional) {
+        return CompletableFuture.completedFuture(Optional.empty());
+    }
+}

90848b8972ca9d6def612246cb74a0bbf3902e7f
embeddedt <42941056+embeddedt@users.noreply.github.com>
2024-04-30 18:04:30 -0400

    Update optimization to 1.20

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/faster_structure_location/StructureCheckMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/faster_structure_location/StructureCheckMixin.java
@@ -3,22 +3,26 @@ package org.embeddedt.modernfix.common.mixin.perf.faster_structure_location;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.core.Registry;
 import net.minecraft.world.level.ChunkPos;
-import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.RandomState;
+import net.minecraft.world.level.chunk.ChunkGeneratorStructureState;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureCheck;
 import net.minecraft.world.level.levelgen.structure.StructureCheckResult;
+import org.embeddedt.modernfix.duck.IStructureCheck;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(StructureCheck.class)
-public class StructureCheckMixin {
-    @Shadow @Final private ChunkGenerator chunkGenerator;
-    @Shadow @Final private long seed;
+public class StructureCheckMixin implements IStructureCheck {
     @Shadow @Final private Registry<Structure> structureConfigs;
-    @Shadow @Final private RandomState randomState;
+
+    private ChunkGeneratorStructureState mfix$structureState;
+
+    @Override
+    public void mfix$setStructureState(ChunkGeneratorStructureState state) {
+        mfix$structureState = state;
+    }
 
     /**
      * @author embeddedt (inspired by 24w04a and Bytzo's comment on https://bugs.mojang.com/browse/MC-249136)
@@ -29,17 +33,19 @@ public class StructureCheckMixin {
     private StructureCheckResult mfix$checkForValidPosition(StructureCheckResult storageResult, ChunkPos chunkPos, Structure structure, boolean skipKnownStructures) {
         if (storageResult != null) {
             return storageResult;
-        } else {
+        } else if(mfix$structureState != null) {
             // Check if any of the placements allow for this structure to be in this chunk
-            var structureHolder = this.structureConfigs.getHolder(this.structureConfigs.getId(structure)).orElseThrow();
-            for (var placement : ((ChunkGeneratorAccessor)this.chunkGenerator).invokeGetPlacementsForStructure(structureHolder, this.randomState)) {
-                if (placement.isStructureChunk(this.chunkGenerator, this.randomState, this.seed, chunkPos.x, chunkPos.z)) {
+            var structureHolder = this.structureConfigs.wrapAsHolder(structure);
+            for (var placement : mfix$structureState.getPlacementsForStructure(structureHolder)) {
+                if (placement.isStructureChunk(mfix$structureState, chunkPos.x, chunkPos.z)) {
                     // Allowed - return null so regular check runs
                     return null;
                 }
             }
             // Not allowed - early exit by returning a non-null value
             return StructureCheckResult.START_NOT_PRESENT;
+        } else {
+            return null;
         }
     }
 }

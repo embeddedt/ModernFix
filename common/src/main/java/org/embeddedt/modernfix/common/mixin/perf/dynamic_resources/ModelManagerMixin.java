17a9f122b12c093cfa84db1bfc45d7d5fa1fafab
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-05-02 20:29:25 -0400

    Spotless

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_resources/ModelManagerMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_resources/ModelManagerMixin.java
@@ -22,7 +22,6 @@ import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.embeddedt.modernfix.duck.IExtendedModelBakery;
 import org.embeddedt.modernfix.duck.IExtendedModelManager;
 import org.embeddedt.modernfix.util.CacheUtil;
-import org.embeddedt.modernfix.util.LambdaMap;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;

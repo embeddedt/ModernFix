2e0ec6ae9ef2c92a5d469a34e38ae86a1b3a1424
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-06-04 22:53:28 -0400

    Add ingredient item value ItemStack deduplication
    Inspired by AllTheLeaks, but done slightly differently to hopefully
    be compatible with more mods

---
+++ forge/src/main/java/org/embeddedt/modernfix/forge/mixin/perf/ingredient_item_deduplication/IngredientMixin.java
@@ -0,0 +1,17 @@
+package org.embeddedt.modernfix.forge.mixin.perf.ingredient_item_deduplication;
+
+import net.minecraft.world.item.crafting.Ingredient;
+import org.embeddedt.modernfix.forge.recipe.IngredientValueDeduplicator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.stream.Stream;
+
+@Mixin(Ingredient.class)
+public class IngredientMixin {
+    @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true, ordinal = 0)
+    private static Stream<? extends Ingredient.Value> injectDeduplicationPass(Stream<? extends Ingredient.Value> stream) {
+        return stream.map(IngredientValueDeduplicator::deduplicate);
+    }
+}
---
+++ forge/src/main/java/org/embeddedt/modernfix/forge/recipe/IngredientValueDeduplicator.java
@@ -0,0 +1,33 @@
+package org.embeddedt.modernfix.forge.recipe;
+
+import it.unimi.dsi.fastutil.Hash;
+import it.unimi.dsi.fastutil.objects.ObjectOpenCustomHashSet;
+import net.minecraft.world.item.ItemStackLinkedSet;
+import net.minecraft.world.item.crafting.Ingredient;
+
+/**
+ * @author embeddedt (original inspiration from Uncandango's AllTheLeaks mod)
+ */
+public class IngredientValueDeduplicator {
+    private static final ObjectOpenCustomHashSet<Ingredient.ItemValue> VALUES = new ObjectOpenCustomHashSet<>(new Hash.Strategy<>() {
+        @Override
+        public int hashCode(Ingredient.ItemValue o) {
+            return o == null ? 0 : ItemStackLinkedSet.TYPE_AND_TAG.hashCode(o.item);
+        }
+
+        @Override
+        public boolean equals(Ingredient.ItemValue a, Ingredient.ItemValue b) {
+            return a == b || a != null && b != null && ItemStackLinkedSet.TYPE_AND_TAG.equals(a.item, b.item) && a.item.getCount() == b.item.getCount();
+        }
+    });
+
+    public static Ingredient.Value deduplicate(Ingredient.Value value) {
+        if (value instanceof Ingredient.ItemValue) {
+            synchronized (VALUES) {
+                return VALUES.addOrGet((Ingredient.ItemValue)value);
+            }
+        } else {
+            return value;
+        }
+    }
+}

3f0057fb0535c6d36889db9f9cf767becdec3a8b
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-06-05 21:36:12 -0400

    Merge remote-tracking branch 'origin/1.20' into 1.21.1

---
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/perf/dynamic_dfu/DataFixTypesMixin.java
@@ -0,0 +1,20 @@
+package org.embeddedt.modernfix.common.mixin.perf.dynamic_dfu;
+
+import com.mojang.datafixers.DataFixer;
+import com.mojang.serialization.Dynamic;
+import net.minecraft.util.datafix.DataFixTypes;
+import org.embeddedt.modernfix.dfu.DFUBlaster;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DataFixTypes.class)
+public class DataFixTypesMixin {
+    @Inject(method = "update(Lcom/mojang/datafixers/DataFixer;Lcom/mojang/serialization/Dynamic;II)Lcom/mojang/serialization/Dynamic;", at = @At(value = "INVOKE", target = "Lcom/mojang/datafixers/DataFixer;update(Lcom/mojang/datafixers/DSL$TypeReference;Lcom/mojang/serialization/Dynamic;II)Lcom/mojang/serialization/Dynamic;"))
+    private void kickOnUpdate(DataFixer fixer, Dynamic<?> input, int version, int newVersion, CallbackInfoReturnable<Dynamic<?>> cir) {
+        if (version < newVersion) {
+            DFUBlaster.kick();
+        }
+    }
+}

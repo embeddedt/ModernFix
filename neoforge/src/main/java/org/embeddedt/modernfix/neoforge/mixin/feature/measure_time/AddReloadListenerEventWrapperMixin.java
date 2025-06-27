f7e964c78e31c6eeb965bf4407c140a4f428c92b
DerCommander323 <volcarlos323@googlemail.com>
2025-06-27 20:14:47 +0200

    Temporarily disable AddReloadListenerEventWrapperMixin

--- neoforge/src/main/java/org/embeddedt/modernfix/neoforge/mixin/feature/measure_time/AddReloadListenerEventWrapperMixin.java
+++ neoforge/src/main/java/org/embeddedt/modernfix/neoforge/mixin/feature/measure_time/AddReloadListenerEventWrapperMixin.java
@@ -1,20 +1,23 @@
 package org.embeddedt.modernfix.neoforge.mixin.feature.measure_time;
 
+/* FIXME: I dont know what class this is
 import net.minecraft.server.packs.resources.PreparableReloadListener;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(targets = "net/neoforged/bus/AddReloadListenerEvent$WrappedStateAwareListener")
+
+@Mixin(targets = "net/neoforged/neoforge/event/AddServerReloadListenersEvent")
 public abstract class AddReloadListenerEventWrapperMixin implements PreparableReloadListener {
     @Shadow @Final private PreparableReloadListener wrapped;
 
     /**
      * @author embeddedt
      * @reason make a proper name show up in ProfiledReloadInstance
-     */
+     *
     @Override
     public String getName() {
         return this.wrapped.getClass().getName();
     }
 }
+*/

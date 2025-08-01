44322a7d07fa77da1c6e18b51b8c1bc60c67bac8
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-08-01 18:40:40 -0400

    Tweak narrator mixin

--- common/src/main/java/org/embeddedt/modernfix/common/mixin/feature/suppress_narrator_stacktrace/GameNarratorMixin.java
+++ common/src/main/java/org/embeddedt/modernfix/common/mixin/feature/suppress_narrator_stacktrace/GameNarratorMixin.java
@@ -1,5 +1,7 @@
 package org.embeddedt.modernfix.common.mixin.feature.suppress_narrator_stacktrace;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.text2speech.Narrator;
 import com.mojang.text2speech.NarratorLinux;
 import com.mojang.text2speech.OperatingSystem;
@@ -8,17 +10,16 @@ import org.embeddedt.modernfix.ModernFix;
 import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @ClientOnlyMixin
 @Mixin(GameNarrator.class)
 public class GameNarratorMixin {
-    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/text2speech/Narrator;getNarrator()Lcom/mojang/text2speech/Narrator;"))
-    private Narrator suppressStacktracePrinting() {
+    @WrapOperation(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/text2speech/Narrator;getNarrator()Lcom/mojang/text2speech/Narrator;"))
+    private Narrator suppressStacktracePrinting(Operation<Narrator> original) {
         try {
             return switch (OperatingSystem.get()) {
                 case LINUX -> new NarratorLinux();
-                default -> Narrator.getNarrator();
+                default -> original.call();
             };
         } catch (Narrator.InitializeException e) {
             ModernFix.LOGGER.warn("Failed to initialize Linux Narrator. Make sure you have libflite installed!");

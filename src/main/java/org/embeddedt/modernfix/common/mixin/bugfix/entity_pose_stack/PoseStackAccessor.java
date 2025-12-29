dbf343bf910d22bff13d670c61343f97733b89ff
embeddedt <42941056+embeddedt@users.noreply.github.com>
2025-12-28 19:24:01 -0500

    Reimplement mixin.bugfix.entity_pose_stack

---
+++ src/main/java/org/embeddedt/modernfix/common/mixin/bugfix/entity_pose_stack/PoseStackAccessor.java
@@ -0,0 +1,13 @@
+package org.embeddedt.modernfix.common.mixin.bugfix.entity_pose_stack;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import org.embeddedt.modernfix.annotation.ClientOnlyMixin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PoseStack.class)
+@ClientOnlyMixin
+public interface PoseStackAccessor {
+    @Accessor("lastIndex")
+    int mfix$getLastIndex();
+}

a24037a9d5e40db9fe1c0f6dd83e7a97f4fed7bf
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-03-23 18:47:55 -0400

    Update to 26.1-rc-3

--- build.gradle.kts
+++ build.gradle.kts
@@ -122,8 +122,7 @@ repositories {
         }
     }
     maven {
-        name = "Maven for PR #2993" // https://github.com/neoforged/NeoForge/pull/2993
-        url = uri("https://prmaven.neoforged.net/NeoForge/pr2993")
+        url = uri("https://prmaven.neoforged.net/NeoForge/pr2879")
         content {
             includeModule("net.neoforged", "neoforge")
             includeModule("net.neoforged", "testframework")

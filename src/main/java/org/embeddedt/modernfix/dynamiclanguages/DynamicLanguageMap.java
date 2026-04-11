d749205427d714a4865155f03c16a48f8e564117
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-04-11 14:39:36 -0400

    Adjust dynamic_languages for better mod compatibility

--- src/main/java/org/embeddedt/modernfix/dynamiclanguages/DynamicLanguageMap.java
+++ src/main/java/org/embeddedt/modernfix/dynamiclanguages/DynamicLanguageMap.java
@@ -10,10 +10,28 @@ import net.minecraft.server.packs.resources.Resource;
 import org.embeddedt.modernfix.ModernFix;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DynamicLanguageMap {
-    public static Map<String, String> forStorage(Map<String, ?> storage) {
+    private static Map<String, Object> createStorage(Map<String, String> rawLanguageContents, List<Resource> languageResources) {
+        Map<String, Object> storage = new HashMap<>(rawLanguageContents);
+        for (var resource : languageResources) {
+            try (var stream = resource.open()) {
+                Language.loadFromJson(stream, (key, value) -> {
+                    if (value != null && value.equals(storage.get(key))) {
+                        storage.put(key, resource);
+                    }
+                });
+            } catch (Exception ignored) {
+            }
+        }
+        return Map.copyOf(storage);
+    }
+
+    public static Map<String, String> forVanillaData(Map<String, String> rawLanguageContents, List<Resource> languageResources) {
+        Map<String, Object> storage = createStorage(rawLanguageContents, languageResources);
         LoadingCache<Resource, Map<String, String>> languageFileContents = CacheBuilder.newBuilder()
                 .softValues()
                 .build(new CacheLoader<>() {

5acb5115b91a086dc7640e1f9767475d1eef7c34
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-04-11 14:14:53 -0400

    Add mixin audit to CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,6 +38,10 @@ legacyForge {
         create("server") {
             server()
         }
+        create("auditClient") {
+            client()
+            jvmArguments.addAll("-Dmodernfix.auditAndExit=true", "-Djava.awt.headless=true")
+        }
     }
 
     mods {

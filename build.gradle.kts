349b06b4a36912b0d28174a26d260b9d451dd96d
embeddedt <42941056+embeddedt@users.noreply.github.com>
2026-07-16 19:32:09 -0400

    Fix publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("net.neoforged.moddev.legacyforge") version("2.0.134")
-    id("me.modmuss50.mod-publish-plugin") version("1.1.0")
+    id("me.modmuss50.mod-publish-plugin") version("2.1.1")
 }
 
 val minecraft_version = rootProject.properties["minecraft_version"].toString()
@@ -222,11 +222,14 @@ publishMods {
         projectSlug = "modernfix"
         accessToken = providers.environmentVariable("CURSEFORGE_TOKEN")
         minecraftVersions.add(minecraft_version)
+        client = true
+        server = true
     }
     modrinth {
         projectId = "nmDcB62a"
         accessToken = providers.environmentVariable("MODRINTH_TOKEN")
         minecraftVersions.add(minecraft_version)
+        environment = CLIENT_OR_SERVER_PREFERS_BOTH
     }
 }
 
